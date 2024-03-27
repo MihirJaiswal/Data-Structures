00a023f95876b7a8034d389ab49bd2c1eb7cf741
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-03-27 17:11:28 +0530

    some pattern programms

---
+++ pattern20.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	
+	for(int i=n; i>=1; i--){
+		
+		for(int k = n-i; k>0; k-- )
+		cout<<" ";
+		
+		for(int j = i ; j>0 ; j--){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+/*
+input = 4
+
+output = 
+         ****
+          ***
+           **
+            *
+*/
---
+++ pattern21.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	
+	for(int i =1; i<=n; i++){
+		for(int k = n-i; k>0; k--)
+			cout<<" ";
+			
+			for(int j = 1;j<=i;j++){
+				cout<<"*";
+			}
+			cout<<endl;
+		
+	}
+
+}
+
+/*
+input = 4
+
+output
+
+   * 
+  **
+ ***
+****
+
+*/
